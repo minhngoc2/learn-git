4112e0db0913f0a43775f7e8de78e0405375600a
MinhITPTIT <minh.itptit@gmail.com>
2021-11-13 16:51:39 +0700

    HOmework2

---
+++ AdvancedWeb/Day2/javascript.js
@@ -0,0 +1,13 @@
+const formLogin = document.querySelectorAll(".form-text input");
+const formLabel = document.querySelectorAll(".form-text label");
+for(let i = 0; i < 2; i++){
+    formLogin[i].addEventListener("mouseover", function(){
+        formLabel[i].classList.add("focus")
+    })
+    formLogin[i].addEventListener("mouseout", function(){
+        if(formLogin[i].value == ""){
+            formLabel[i].classList.remove("focus");
+        }
+       
+    })
+}
