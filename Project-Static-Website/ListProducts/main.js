4afd9bd38c41b63794fca607c5d609dd43c8c554
MinhITPTIT <minh.itptit@gmail.com>
2022-02-20 10:14:31 +0700

    blogpage

---
+++ Project-Static-Website/ListProducts/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $("#sort").click(function () {
+    $("#menu").fadeToggle(1000);
+  })
+  $("#filter").click(function () {
+    $("#row-filter").slideToggle(1000);
+  })
+})
